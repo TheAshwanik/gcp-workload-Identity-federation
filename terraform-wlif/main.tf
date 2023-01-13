dcd35f39cea1ca5a8336812b4831c9240ba299c1
TheAshwanik <2206818+TheAshwanik@users.noreply.github.com>
2023-01-13 11:37:39 +0000

    added outputs in wlif terraform code

--- terraform-wlif/main.tf
+++ terraform-wlif/main.tf
@@ -1,11 +1,11 @@
 resource "google_iam_workload_identity_pool" "gh_pool" {
+  provider 		    = google-beta
   project                   = var.project_id
-  provider                  = google-beta
   workload_identity_pool_id = "gh-pool"
 }
 
 resource "google_iam_workload_identity_pool_provider" "provider" {
-  provider                           = google-beta
+  provider 			     = google-beta
   project                            = var.project_id
   workload_identity_pool_id          = google_iam_workload_identity_pool.gh_pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "gh-provider"
--- terraform-wlif/outputs.tf
+++ terraform-wlif/outputs.tf
@@ -6,3 +6,14 @@ output "project_number" {
   description = "Project number"
   value       = "${data.google_project.project.number}"
 }
+
+data "google_iam_workload_identity_pool_provider" "provider" {
+  provider                           = google-beta
+  workload_identity_pool_provider_id = "gh-provider"
+  workload_identity_pool_id = "gh-pool"
+}
+
+output "provider" {
+  description = "Provider id"
+  value       = "${data.google_iam_workload_identity_pool_provider.provider.id}"
+}
